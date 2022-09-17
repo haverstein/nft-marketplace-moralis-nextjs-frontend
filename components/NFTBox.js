c3a7096ed3d4f7837c68c544fff4eb97cec8e909
haverstein <haverstein3@gmail.com>
2022-09-17 15:31:37 +0530

    nft-from-ipfs-into-frontend

--- components/NFTBox.js
+++ components/NFTBox.js
@@ -2,10 +2,15 @@ import { useEffect, useState } from "react"
 import { useWeb3Contract, useMoralis } from "react-moralis"
 import nftMarketplaceAbi from "../constants/NftMarketplace.json"
 import basicNftAbi from "../constants/BasicNft.json"
+import Image from "next/image"
+import { Card } from "web3uikit"
+import { ethers } from "ethers"
 
 export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress, seller }) {
     const { isWeb3Enabled } = useMoralis()
     const [imageUri, setImageUri] = useState("")
+    const [tokenName, setTokenName] = useState("")
+    const [tokenDescription, setTokenDescription] = useState("")
     const { runContractFunction: getTokenUri } = useWeb3Contract({
         abi: basicNftAbi,
         contractAddress: nftAddress,
@@ -16,11 +21,50 @@ export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress,
     })
     async function updateUi() {
         const tokenURI = await getTokenUri()
-        console.log(tokenURI)
+        console.log(`The token uri is ${tokenURI}`)
+        if (tokenURI) {
+            // IPFS Gateway: A server that will return IPFS files from a normal url.
+            const requestUrl = tokenURI.replace("ipfs://", "https://ipfs.io/ipfs/")
+            const tokenURIResponse = await (await fetch(requestUrl)).json()
+            const imageURI = tokenURIResponse.image
+            const imageUrl = imageURI.replace("ipfs://", "https://ipfs.io/ipfs/")
+            setImageUri(imageUrl)
+            setTokenName(tokenURIResponse.name)
+            setTokenDescription(tokenURIResponse.description)
+            // Alternate way: 1. We could render image on our server and call our server(Moralis)
+            // 2. use Moralis Hooks
+        }
     }
     useEffect(() => {
         if (isWeb3Enabled) {
             updateUi()
         }
     }, [isWeb3Enabled])
+    return (
+        <div>
+            <div>
+                {imageUri ? (
+                    <Card title={tokenName} description={tokenDescription}>
+                        <div className="p-2">
+                            <div className="flex flex-col items-end gap-2">
+                                <div>#{tokenId}</div>
+                                <div className="italic text-sn">Owned by {seller}</div>
+                                <Image
+                                    loader={() => imageUri}
+                                    src={imageUri}
+                                    height="200"
+                                    width="200"
+                                />
+                                <div className="font-bold">
+                                    {ethers.utils.formatUnits(price, "ether")} ETH
+                                </div>
+                            </div>
+                        </div>
+                    </Card>
+                ) : (
+                    <div>Loading...</div>
+                )}
+            </div>
+        </div>
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,49 @@
 import styles from "../styles/Home.module.css"
-import { useMoralisQuery } from "react-moralis"
+import { useMoralis, useMoralisQuery } from "react-moralis"
 import NFTBox from "../components/NFTBox"
 
 export default function Home() {
+    const { isWeb3Enabled } = useMoralis()
     const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
         "ActiveItem",
         (query) => query.limit(10).descending("tokenId")
     )
     return (
-        <div className={styles.container}>
-            {fetchingListedNfts ? (
-                <div>Loading...</div>
-            ) : (
-                listedNfts.map((nft) => {
-                    const { price, nftAddress, tokenId, marketplaceAddress, seller } =
-                        nft.attributes
-                    return (
-                        <div>
-                            Price : {price}. NftAddress : {nftAddress}. TokenId : {tokenId}. seller
-                            : {seller}
-                            <NFTBox
-                                price={price}
-                                nftAddress={nftAddress}
-                                tokenId={tokenId}
-                                marketplaceAddress={marketplaceAddress}
-                                seller={seller}
-                                key={`${nftAddress}${tokenId}`}
-                            />
-                        </div>
+        <div className="container mx-auto">
+            <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
+            <div className="flex flex-wrap">
+                {isWeb3Enabled ? (
+                    fetchingListedNfts ? (
+                        <div>Loading...</div>
+                    ) : (
+                        listedNfts.map((nft) => {
+                            const { price, nftAddress, tokenId, marketplaceAddress, seller } =
+                                nft.attributes
+                            return (
+                                <div>
+                                    <NFTBox
+                                        price={price}
+                                        nftAddress={nftAddress}
+                                        tokenId={tokenId}
+                                        marketplaceAddress={marketplaceAddress}
+                                        seller={seller}
+                                        key={`${nftAddress}${tokenId}`}
+                                    />
+                                </div>
+                            )
+                        })
                     )
-                })
-            )}
+                ) : (
+                    <div className="p-4">
+                        Web3 Currently not enabled.
+                        <br />
+                        <small>
+                            Tip:<i>Connect with metamask :)</i>
+                        </small>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
+//27.13.16
